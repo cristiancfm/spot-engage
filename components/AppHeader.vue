086864d789bedb81baccb9f5fbe2daf1ed9c79c6
Cristian Ferreiro Montoiro <cristian.ferreiro@udc.es>
2025-03-26 12:49:55 +0100

    Added basic structure and ESLint / Prettier modules

---
+++ components/AppHeader.vue
@@ -0,0 +1,12 @@
+<template>
+	<header>
+		<h1>App Header</h1>
+		<a>kasdkf</a>
+		<nav>
+			<ul>
+				<li><NuxtLink to="/">Home</NuxtLink></li>
+				<li><NuxtLink to="/about">About</NuxtLink></li>
+			</ul>
+		</nav>
+	</header>
+</template>
---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+
+export default withNuxt(
+	{
+		plugins: {
+			prettier: eslintPluginPrettier,
+		},
+		rules: {
+			'prettier/prettier': [
+				'error',
+				{
+					endOfLine: 'auto',
+				},
+			],
+		},
+	},
+
+	{
+		files: ['**/*.ts', '**/*.vue', '**/*.js'],
+		rules: {
+			'@typescript-eslint/no-explicit-any': 'warn',
+			'@typescript-eslint/no-unused-vars': 'off',
+
+			'vue/multi-word-component-names': 'off',
+			'vue/html-self-closing': 'off',
+
+			'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+			'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		},
+	}
+);
