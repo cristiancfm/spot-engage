edcba75ffd630d05b0cb6dc1e928f26c95518baa
Cristian Ferreiro Montoiro <cristian.ferreiro@udc.es>
2025-04-10 21:14:56 +0200

    Improved homepage and navbar

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,12 +1,78 @@
 <template>
-  <header></header>
+  <header>
+    <!-- Desktop Menu -->
+    <v-toolbar app border class="d-none d-sm-flex">
+      <v-toolbar-title>
+        <router-link to="/" class="mx-2">
+          <v-img src="/favicon.ico" alt="Logo" max-height="50" max-width="50" />
+        </router-link>
+      </v-toolbar-title>
+      <v-spacer />
+
+      <v-toolbar-items>
+        <v-btn>{{ $t("menu.venues") }}</v-btn>
+        <v-btn :to="{ name: 'about' }">{{ $t("menu.about") }}</v-btn>
+        <v-btn>{{ $t("menu.venueLogin") }}</v-btn>
+        <v-btn>{{ $t("menu.customerLogin") }}</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <!-- Mobile Menu -->
+    <v-toolbar app border class="d-flex d-sm-none">
+      <v-toolbar-title>
+        <router-link to="/" class="mx-2">
+          <v-img src="/favicon.ico" alt="Logo" max-height="50" max-width="50" />
+        </router-link>
+      </v-toolbar-title>
+      <v-spacer />
+      <template #append>
+        <v-btn icon="menu" @click="dialog = !dialog" />
+      </template>
+
+      <v-dialog
+        v-model="dialog"
+        light
+        fullscreen
+        :scrim="false"
+        transition="dialog-bottom-transition"
+      >
+        <v-card light>
+          <v-toolbar border>
+            <v-toolbar-title>Menu</v-toolbar-title>
+            <v-spacer />
+            <v-btn icon="close" @click="dialog = false" />
+          </v-toolbar>
+
+          <v-list>
+            <v-list-item to="/" @click="dialog = false">
+              <v-list-item-title>{{ $t("menu.home") }}</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="dialog = false">
+              <v-list-item-title>{{ $t("menu.venues") }}</v-list-item-title>
+            </v-list-item>
+            <v-list-item :to="{ name: 'about' }" @click="dialog = false">
+              <v-list-item-title>{{ $t("menu.about") }}</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="dialog = false">
+              <v-list-item-title>{{ $t("menu.customerLogin") }}</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="dialog = false">
+              <v-list-item-title>{{ $t("menu.venueLogin") }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-dialog>
+    </v-toolbar>
+  </header>
 </template>
 
 <script>
 export default {
   name: "AppHeader",
   data() {
-    return {};
+    return {
+      dialog: false,
+    };
   },
   methods: {
     setLocale(locale) {
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="text-center p-4">
-    <h1>{{ $t("index.title") }}</h1>
-    <h2>{{ $t("index.description", { appName: $t("appName") }) }}</h2>
-    <v-icon>settings</v-icon>
-    <v-btn>{{ $t("index.primaryButton") }}</v-btn>
-    <v-btn>{{ $t("index.secondaryButton") }}</v-btn>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col>
+        <h1 class="font-weight-black">{{ $t("index.title") }}</h1>
+        <h2 class="font-weight-regular">
+          {{ $t("index.description", { appName: $t("appName") }) }}
+        </h2>
+      </v-col>
+    </v-row>
+    <v-row class="text-center">
+      <v-col>
+        <v-btn variant="flat" color="primary" class="ma-2">{{
+          $t("index.primaryButton")
+        }}</v-btn>
+        <v-btn variant="flat" color="secondary" class="ma-2">{{
+          $t("index.secondaryButton")
+        }}</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
