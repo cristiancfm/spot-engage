c7520a644e67e7edce2b23eae723906398038886
Cristian Ferreiro Montoiro <cristian.ferreiro@udc.es>
2025-04-21 16:58:15 +0200

    Added persistence to pinia

--- store/website.ts
+++ store/website.ts
@@ -1,6 +1,13 @@
+import { defineStore } from 'pinia';
+
 export const useWebsiteStore = defineStore('website', {
   state: () => ({
-    accessCode: "",
+    accessCode: "ABC-DEF",
     token: "",
-  })
+    spotifyClientId: import.meta.env.VITE_SPOTIFY_CLIENT_ID || "",
+    spotifyToken: "",
+  }),
+  persist: {
+    storage: piniaPluginPersistedstate.localStorage(),
+  },
 });
