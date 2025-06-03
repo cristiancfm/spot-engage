f1c41fa6f3ce4e61f4740d86cc6790ef683d60d3
Cristian Ferreiro Montoiro <cristian.ferreiro@udc.es>
2025-06-03 12:57:37 +0200

    Removed unused function

--- composables/useSpotify.js
+++ composables/useSpotify.js
@@ -1,11 +1,4 @@
 export function useSpotify() {
-  const fetchProfile = async (token) => {
-    return $fetch("https://api.spotify.com/v1/me", {
-      method: "GET",
-      headers: { Authorization: `Bearer ${token}` },
-    });
-  };
-
   const fetchQueue = async (token) => {
     return $fetch("https://api.spotify.com/v1/me/player/queue", {
       method: "GET",
@@ -67,7 +60,6 @@ export function useSpotify() {
   };
 
   return {
-    fetchProfile,
     fetchQueue,
     fetchTracks,
     submitTrackToQueue,
